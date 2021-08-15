b855a3ecff208888d216abcd677bcd8a20ce4a71
⎛⎝Trek⎠⎞ <asd198301@yandex.ru>
2021-08-15 20:46:23 +0300

    Update grenade_frag.cpp
    Fixed server crash way, when player throws and takes grenade many times.

--- mp/src/game/server/hl2/grenade_frag.cpp
+++ mp/src/game/server/hl2/grenade_frag.cpp
@@ -155,7 +155,8 @@ void CGrenadeFrag::OnRestore( void )
 void CGrenadeFrag::CreateEffects( void )
 {
 	// Start up the eye glow
-	m_pMainGlow = CSprite::SpriteCreate( "sprites/redglow1.vmt", GetLocalOrigin(), false );
+	if( !m_pMainGlow )
+		m_pMainGlow = CSprite::SpriteCreate( "sprites/redglow1.vmt", GetLocalOrigin(), false );
 
 	int	nAttachment = LookupAttachment( "fuse" );
 
@@ -180,7 +181,8 @@ void CGrenadeFrag::CreateEffects( void )
 	}
 
 	// Start up the eye trail
-	m_pGlowTrail	= CSpriteTrail::SpriteTrailCreate( "sprites/bluelaser1.vmt", GetLocalOrigin(), false );
+	if( !m_pGlowTrail )
+		m_pGlowTrail	= CSpriteTrail::SpriteTrailCreate( "sprites/bluelaser1.vmt", GetLocalOrigin(), false );
 
 	if ( m_pGlowTrail != NULL )
 	{
