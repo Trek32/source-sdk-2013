f864f6b2711b59201a06f94cfe338f95143753a3
⎛⎝Trek⎠⎞ <asd198301@yandex.ru>
2022-01-16 05:23:47 +0300

    Update weapon_rpg.cpp
    Fixed a bug in the console, meaning that the sprite format was not specified;
    Removed double precaching.

--- mp/src/game/shared/hl2mp/weapon_rpg.cpp
+++ mp/src/game/shared/hl2mp/weapon_rpg.cpp
@@ -1257,7 +1257,7 @@ void CAPCMissile::ComputeActualDotPosition( CLaserDot *pLaserDot, Vector *pActua
 
 #define	RPG_BEAM_SPRITE		"effects/laser1.vmt"
 #define	RPG_BEAM_SPRITE_NOZ	"effects/laser1_noz.vmt"
-#define	RPG_LASER_SPRITE	"sprites/redglow1"
+#define	RPG_LASER_SPRITE	"sprites/redglow1.vmt"
 
 //=============================================================================
 // RPG
@@ -1370,7 +1370,6 @@ void CWeaponRPG::Precache( void )
 	PrecacheScriptSound( "Missile.Accelerate" );
 
 	// Laser dot...
-	PrecacheModel( "sprites/redglow1.vmt" );
 	PrecacheModel( RPG_LASER_SPRITE );
 	PrecacheModel( RPG_BEAM_SPRITE );
 	PrecacheModel( RPG_BEAM_SPRITE_NOZ );
